f3435f2fdd19b92d9c0691c11ca7a060d1686901
anguaive <angurg98@gmail.com>
2021-03-25 10:47:36 +0100

    Day 17 Part 2 Rust solution

--- 2020/rust/src/day17.rs
+++ 2020/rust/src/day17.rs
@@ -2,6 +2,11 @@ use std::convert::TryInto;
 use std::error::Error;
 use std::fmt;
 
+enum Part {
+    One,
+    Two,
+}
+
 #[derive(Debug)]
 enum ParseUniverseError {
     Value,
@@ -18,14 +23,13 @@ impl fmt::Display for ParseUniverseError {
 impl Error for ParseUniverseError {}
 
 struct Universe {
-    planes: Vec<Vec<Cell>>,
+    planes: Vec<Vec<Vec<Cell>>>,
     x: usize,
     y: usize,
-    z: usize,
 }
 
 impl Universe {
-    fn new<'a, I, S>(lines: I, cycles: usize) -> Result<Self, ParseUniverseError>
+    fn new<'a, I, S>(lines: I, cycles: usize, part: Part) -> Result<Self, ParseUniverseError>
     where
         I: IntoIterator<Item = &'a S>,
         S: AsRef<str> + 'a,
@@ -50,9 +54,13 @@ impl Universe {
         let new_x = x + 2 * cycles;
         let new_y = y + 2 * cycles;
         let z = 1 + 2 * cycles;
+        let w = match part {
+            Part::One => 1,
+            Part::Two => 1 + 2 * cycles,
+        };
 
         let empty_plane = vec![Cell::Inactive; new_x * new_y];
-        let mut planes = vec![empty_plane.clone(); z];
+        let mut planes = vec![vec![empty_plane.clone(); z]; w];
 
         let mut starting_plane = empty_plane.clone();
         cells
@@ -67,41 +75,46 @@ impl Universe {
                 starting_plane[idx] = c;
             });
 
-        planes[cycles] = starting_plane;
+        planes[(w - 1) / 2][cycles] = starting_plane;
 
         Ok(Universe {
             planes,
             x: new_x,
             y: new_y,
-            z,
         })
     }
 
     pub fn get_cell_count(&self, cell_type: Cell) -> usize {
         self.planes
             .iter()
-            .map(|p| p.iter().filter(|&c| *c == cell_type).count())
+            .map(|s| {
+                s.iter()
+                    .map(|p| p.iter().filter(|&c| *c == cell_type).count())
+                    .sum::<usize>()
+            })
             .sum()
     }
 
     pub fn tick(&mut self) {
         let mut new_planes = self.planes.clone();
 
-        for z in 0..self.planes.len() {
-            for i in 0..self.x * self.y {
-                let neighbors = self.get_neighbors(z, i);
-                let cell = self.get_cell(z, i);
+        for w in 0..self.planes.len() {
+            for z in 0..self.planes[w].len() {
+                for i in 0..self.x * self.y {
+                    let neighbors = self.get_neighbors(w, z, i);
+                    let cell = self.get_cell(w, z, i);
 
-                match *cell {
-                    Cell::Inactive => {
-                        if neighbors.iter().filter(|&&n| *n == Cell::Active).count() == 3 {
-                            new_planes[z][i] = Cell::Active;
+                    match *cell {
+                        Cell::Inactive => {
+                            if neighbors.iter().filter(|&&n| *n == Cell::Active).count() == 3 {
+                                new_planes[w][z][i] = Cell::Active;
+                            }
                         }
-                    }
-                    Cell::Active => {
-                        let count = neighbors.iter().filter(|&&n| *n == Cell::Active).count();
-                        if count != 2 && count != 3 {
-                            new_planes[z][i] = Cell::Inactive;
+                        Cell::Active => {
+                            let count = neighbors.iter().filter(|&&n| *n == Cell::Active).count();
+                            if count != 2 && count != 3 {
+                                new_planes[w][z][i] = Cell::Inactive;
+                            }
                         }
                     }
                 }
@@ -111,86 +124,74 @@ impl Universe {
         self.planes = new_planes;
     }
 
-    fn get_cell(&self, z: usize, i: usize) -> &Cell {
-        &self.planes[z][i]
+    fn get_cell(&self, w: usize, z: usize, i: usize) -> &Cell {
+        &self.planes[w][z][i]
     }
 
-    fn get_neighbors(&self, z: usize, i: usize) -> Vec<&Cell> {
+    fn get_neighbors(&self, w: usize, z: usize, i: usize) -> Vec<&Cell> {
         let z: i32 = z.try_into().unwrap(); // Safe because z is small
+        let w: i32 = w.try_into().unwrap(); // Safe because w is small
         let mut neighbors = vec![];
-        for z_offset in [-1, 0, 1].into_iter() {
-            let z_idx = z + z_offset;
-            if z_idx < 0 || self.planes.len() <= z_idx as usize {
+        for w_offset in [-1, 0, 1].iter() {
+            let w_idx = w + w_offset;
+            if w_idx < 0 || self.planes.len() <= w_idx as usize {
                 continue;
             }
 
-            if let Some(plane) = self.planes.get(z_idx as usize) {
-                let left = i % self.x != 0;
-                let right = i % self.x != self.x - 1;
-                let up = i >= self.x;
-                let down = i <= (self.y - 1) * self.x - 1;
-
-                if left {
-                    neighbors.push(&plane[i - 1]);
+            for z_offset in [-1, 0, 1].iter() {
+                let z_idx = z + z_offset;
+                if z_idx < 0 || self.planes[w_idx as usize].len() <= z_idx as usize {
+                    continue;
                 }
 
-                if right {
-                    neighbors.push(&plane[i + 1]);
-                }
+                if let Some(space) = self.planes.get(w_idx as usize) {
+                    if let Some(plane) = space.get(z_idx as usize) {
+                        let left = i % self.x != 0;
+                        let right = i % self.x != self.x - 1;
+                        let up = i >= self.x;
+                        let down = i <= (self.y - 1) * self.x - 1;
 
-                if up {
-                    neighbors.push(&plane[i - self.x]);
-                }
+                        if left {
+                            neighbors.push(&plane[i - 1]);
+                        }
 
-                if down {
-                    neighbors.push(&plane[i + self.x]);
-                }
+                        if right {
+                            neighbors.push(&plane[i + 1]);
+                        }
 
-                if left && up {
-                    neighbors.push(&plane[i - self.x - 1]);
-                }
+                        if up {
+                            neighbors.push(&plane[i - self.x]);
+                        }
 
-                if right && up {
-                    neighbors.push(&plane[i - self.x + 1]);
-                }
+                        if down {
+                            neighbors.push(&plane[i + self.x]);
+                        }
 
-                if left && down {
-                    neighbors.push(&plane[i + self.x - 1]);
-                }
+                        if left && up {
+                            neighbors.push(&plane[i - self.x - 1]);
+                        }
 
-                if right && down {
-                    neighbors.push(&plane[i + self.x + 1]);
-                }
+                        if right && up {
+                            neighbors.push(&plane[i - self.x + 1]);
+                        }
 
-                if *z_offset != 0 {
-                    neighbors.push(&plane[i]);
+                        if left && down {
+                            neighbors.push(&plane[i + self.x - 1]);
+                        }
+
+                        if right && down {
+                            neighbors.push(&plane[i + self.x + 1]);
+                        }
+
+                        if *z_offset != 0 || *w_offset != 0 {
+                            neighbors.push(&plane[i]);
+                        }
+                    }
                 }
             }
         }
 
         neighbors
-    }
-}
-
-impl fmt::Display for Universe {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        let mut z: i32 = self.z.try_into().unwrap();
-        z /= 2;
-        write!(f, "{}x{}, height: {}\n", self.x, self.y, self.z)?;
-        for plane in self.planes.iter() {
-            write!(f, "Plane {}:\n", z)?;
-            for row in plane.chunks(self.x) {
-                for cell in row {
-                    write!(f, "{}", cell)?;
-                }
-                write!(f, "\n")?;
-            }
-            write!(f, "\n")?;
-
-            z -= 1;
-        }
-
-        Ok(())
     }
 }
 
@@ -200,21 +201,12 @@ enum Cell {
     Inactive,
 }
 
-impl fmt::Display for Cell {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        match *self {
-            Cell::Active => write!(f, "# "),
-            Cell::Inactive => write!(f, ". "),
-        }
-    }
-}
-
 pub fn part1<'a, I, S>(lines: I) -> Option<usize>
 where
     I: IntoIterator<Item = &'a S>,
     S: AsRef<str> + 'a,
 {
-    let mut u = Universe::new(lines, 6).ok()?;
+    let mut u = Universe::new(lines, 6, Part::One).ok()?;
 
     for _ in 0..6 {
         u.tick();
@@ -223,12 +215,18 @@ where
     Some(u.get_cell_count(Cell::Active))
 }
 
-pub fn part2<'a, I, S>(_lines: I) -> Option<u32>
+pub fn part2<'a, I, S>(lines: I) -> Option<usize>
 where
     I: IntoIterator<Item = &'a S>,
     S: AsRef<str> + 'a,
 {
-    todo!()
+    let mut u = Universe::new(lines, 6, Part::Two).ok()?;
+
+    for _ in 0..6 {
+        u.tick();
+    }
+
+    Some(u.get_cell_count(Cell::Active))
 }
 
 #[cfg(test)]
@@ -248,6 +246,6 @@ mod tests {
     fn part2_test() {
         let result = part2(EXAMPLE).unwrap();
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 848);
     }
 }
