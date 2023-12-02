d3351af0e612906f40959dffb1456394a61a64a3
sij1nk <gergo.rajnavolgyi@gmail.com>
2023-12-02 17:17:28 +0100

    Generate template for 2023 day 2

--- 2023/rust/benches/criterion.rs
+++ 2023/rust/benches/criterion.rs
@@ -2,7 +2,7 @@
 extern crate build_const;
 
 #[rustfmt::skip]
-use aoc2023::{day1, /*%criterion.rs_import%*/};
+use aoc2023::{day1, day2, /*%criterion.rs_import%*/};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
 build_const!("aoc2023");
@@ -10,6 +10,8 @@ build_const!("aoc2023");
 fn aoc2023_bench(c: &mut Criterion) {
     c.bench_function("day 1 part 1", |b| b.iter(|| day1::part1(black_box(DAY1))));
     c.bench_function("day 1 part 2", |b| b.iter(|| day1::part2(black_box(DAY1))));
+    c.bench_function("day 2 part 1", |b| b.iter(|| day2::part1(black_box(DAY2))));
+    c.bench_function("day 2 part 2", |b| b.iter(|| day2::part2(black_box(DAY2))));
     /*%criterion.rs_call%*/
 }
 
--- 2023/rust/src/main.rs
+++ 2023/rust/src/main.rs
@@ -3,7 +3,7 @@ use std::collections::HashSet;
 use std::env;
 
 #[rustfmt::skip]
-use aoc2023::{day1, /*%main.rs_import%*/};
+use aoc2023::{day1, day2, /*%main.rs_import%*/};
 
 build_const!("aoc2023");
 
@@ -16,6 +16,11 @@ fn main() -> anyhow::Result<()> {
         println!("{:?}", day1::part2(DAY1)?);
     }
 
+    if args.is_empty() || args.contains("2") {
+        println!("Day 2");
+        println!("{:?}", day2::part1(DAY2)?);
+        println!("{:?}", day2::part2(DAY2)?);
+    }
     /*%main.rs_call%*/
 
     Ok(())
