644f215a0d78d45c14a1b4687ed91c0d67179193
sij1nk <gergo.rajnavolgyi@gmail.com>
2023-12-02 17:14:46 +0100

    Implement new day bootstrapping
    I basically copied what I did in 2020, with a few adjustments:
    - remove leading zero confusion
    - abort if day argument is invalid
    - abort if source files for the given day are already present
    - abort if api key file is missing

--- 2023/rust/scripts/setup-day.sh
+++ 2023/rust/scripts/setup-day.sh
@@ -26,3 +26,43 @@ then
   rm ${filename}
   exit 1
 fi
+
+# Begin patching files...
+lib_file="src/lib.rs"
+main_file="src/main.rs"
+bench_file="benches/criterion.rs"
+src_file="src/day${day}.rs"
+template_file=".template.rs"
+
+bench_call_template="$(cat << 'EOF'
+c.bench_function("day %DAY% part 1", |b| b.iter(||
+day%DAY%::part1(black_box(DAY%DAY%))));
+c.bench_function("day %DAY% part 2", |b| b.iter(||
+day%DAY%::part2(black_box(DAY%DAY%))));
+EOF
+)"
+bench_call_template="$(echo "${bench_call_template}" | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/\$/\\$/g')"
+
+main_call_template="$(cat << 'EOF'
+if args.is_empty() || args.contains("%DAY%") {
+println!("Day %DAY%");
+println!("{:?}", day%DAY%::part1(DAY%DAY%)?);
+println!("{:?}", day%DAY%::part2(DAY%DAY%)?);
+}
+EOF
+)"
+main_call_template="$(echo "${main_call_template}" | sed ':a;N;$!ba;s/\n/\\n/g' | sed 's/\$/\\$/g')"
+
+cp $template_file $src_file
+
+sed -i "s/\/\*%lib.rs_module%\*\//pub mod day%DAY%;\n\/\*%lib.rs_module%\*\//" $lib_file
+sed -i "s/\/\*%main.rs_call%\*\//$main_call_template\n\/\*%main.rs_call%\*\//" $main_file
+sed -i "s/\/\*%main.rs_import%\*\//day%DAY%, \/\*%main.rs_import%\*\//" $main_file
+sed -i "s/\/\*%criterion.rs_call%\*\//$bench_call_template\n\/\*%criterion.rs_call%\*\//" $bench_file
+sed -i "s/\/\*%criterion.rs_import%\*\//day%DAY%, \/\*%criterion.rs_import%\*\//" $bench_file
+
+sed -i "s/%DAY%/${day}/g" $lib_file
+sed -i "s/%DAY%/${day}/g" $main_file
+sed -i "s/%DAY%/${day}/g" $bench_file
+
+rustfmt $lib_file $main_file $bench_file
