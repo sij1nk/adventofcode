c7df128173692794480b4b270401f007d56dc672
sij1nk <gergo.rajnavolgyi@gmail.com>
2023-12-08 07:36:19 +0100

    Add 2023 day 7 part 2 solution

--- 2023/rust/src/day7.rs
+++ 2023/rust/src/day7.rs
@@ -1,5 +1,5 @@
 use anyhow::anyhow;
-use std::{collections::BTreeMap, str::FromStr};
+use std::collections::BTreeMap;
 
 #[derive(Clone, Copy)]
 enum HandType {
@@ -42,31 +42,66 @@ const LEXICAL_TRANSFORM_TABLE: &[(char, char)] = &[
     ('2', 'N'),
 ];
 
+const LEXICAL_TRANSFORM_TABLE_PART2: &[(char, char)] = &[
+    ('A', 'Z'),
+    ('K', 'Y'),
+    ('Q', 'X'),
+    ('T', 'V'),
+    ('9', 'U'),
+    ('8', 'T'),
+    ('7', 'S'),
+    ('6', 'R'),
+    ('5', 'Q'),
+    ('4', 'P'),
+    ('3', 'O'),
+    ('2', 'N'),
+    ('J', 'A'),
+];
+
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
 struct Hand {
     value: String,
 }
 
-fn get_unique_cards_count(s: &str) -> u32 {
+fn get_unique_cards_count(s: &str, consider_jokers: bool) -> u32 {
     let mut count = 0;
     let mut array = ['0'; 5];
 
     for c in s.chars() {
+        if consider_jokers && c == 'J' {
+            continue;
+        }
+
         if !array.contains(&c) {
             array[count] = c;
             count += 1;
         }
     }
 
-    count as u32
+    // JJJJJ corner case for part 2
+    if count == 0 {
+        1
+    } else {
+        count as u32
+    }
 }
 
-fn get_highest_card_occurrence(s: &str) -> u32 {
+fn get_highest_card_occurrence(s: &str, consider_jokers: bool) -> u32 {
+    let jokers_count = if consider_jokers {
+        s.chars().filter(|&c| c == 'J').count()
+    } else {
+        0
+    };
+
     let mut highest = 0;
     let mut highest_char = None;
-    let mut rem = 5;
+    let mut rem = 5 - jokers_count;
 
     for char in s.chars() {
+        if consider_jokers && char == 'J' {
+            continue;
+        }
+
         if highest_char.is_some_and(|c| c == char) {
             continue;
         }
@@ -84,20 +119,20 @@ fn get_highest_card_occurrence(s: &str) -> u32 {
         }
     }
 
-    highest as u32
+    (highest + jokers_count) as u32
 }
 
-fn determine_hand_type(s: &str) -> HandType {
-    let unique_count = get_unique_cards_count(s);
+fn determine_hand_type(s: &str, consider_jokers: bool) -> HandType {
+    let unique_count = get_unique_cards_count(s, consider_jokers);
 
     match unique_count {
         1 => HandType::FiveOfAKind,
-        2 => match get_highest_card_occurrence(s) {
+        2 => match get_highest_card_occurrence(s, consider_jokers) {
             3 => HandType::FullHouse,
             4 => HandType::FourOfAKind,
             _ => panic!("Incorrect highest card occurrence number for unique count 2"),
         },
-        3 => match get_highest_card_occurrence(s) {
+        3 => match get_highest_card_occurrence(s, consider_jokers) {
             2 => HandType::TwoPair,
             3 => HandType::ThreeOfAKind,
             _ => panic!("Incorrect highest card occurrence number for unique count 3"),
@@ -108,28 +143,32 @@ fn determine_hand_type(s: &str) -> HandType {
     }
 }
 
-impl FromStr for Hand {
-    type Err = anyhow::Error;
-
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+impl Hand {
+    fn new(s: &str, consider_jokers: bool) -> Self {
         let mut value = String::with_capacity(6);
 
-        let hand_type = determine_hand_type(s);
+        let hand_type = determine_hand_type(s, consider_jokers);
         value.push(hand_type.to_char());
 
+        let transform_table = if consider_jokers {
+            LEXICAL_TRANSFORM_TABLE_PART2
+        } else {
+            LEXICAL_TRANSFORM_TABLE
+        };
+
         for c in s.chars() {
-            let transform = LEXICAL_TRANSFORM_TABLE
+            let transform = transform_table
                 .iter()
                 .find(|&item| item.0 == c)
                 .expect("Char could not be translated");
             value.push(transform.1);
         }
 
-        Ok(Hand { value })
+        Hand { value }
     }
 }
 
-pub fn part1<'a, I, S>(lines: I) -> anyhow::Result<u64>
+fn calculate_winnings<'a, I, S>(lines: I, consider_jokers: bool) -> anyhow::Result<u64>
 where
     I: IntoIterator<Item = &'a S>,
     S: AsRef<str> + 'a,
@@ -144,7 +183,7 @@ where
         let (hand_str, winnings_str) = line
             .split_once(' ')
             .ok_or(anyhow!("Could not split line by ' '"))?;
-        let hand = Hand::from_str(hand_str)?;
+        let hand = Hand::new(hand_str, consider_jokers);
         let winnings = winnings_str.parse::<u64>()?;
 
         hands.insert(hand, winnings);
@@ -156,12 +195,20 @@ where
         .fold(0, |acc, (i, w)| acc + (i + 1) as u64 * w))
 }
 
-pub fn part2<'a, I, S>(_lines: I) -> anyhow::Result<u32>
+pub fn part1<'a, I, S>(lines: I) -> anyhow::Result<u64>
 where
     I: IntoIterator<Item = &'a S>,
     S: AsRef<str> + 'a,
 {
-    todo!()
+    calculate_winnings(lines, false)
+}
+
+pub fn part2<'a, I, S>(lines: I) -> anyhow::Result<u64>
+where
+    I: IntoIterator<Item = &'a S>,
+    S: AsRef<str> + 'a,
+{
+    calculate_winnings(lines, true)
 }
 
 #[cfg(test)]
@@ -187,6 +234,6 @@ mod tests {
     fn part2_test() {
         let result = part2(EXAMPLE).unwrap();
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 5905);
     }
 }
