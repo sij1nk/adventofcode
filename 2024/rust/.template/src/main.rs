5962aa3632f748a414aba5049f25ab5c5640d1ca
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-12-21 21:56:30 +0100

    Finalize init-day script and template

--- 2024/rust/.template/src/main.rs
+++ 2024/rust/.template/src/main.rs
@@ -3,7 +3,7 @@ use %DAY%::{read_input, solution};
 fn main() -> anyhow::Result<()> {
     let input = read_input()?;
 
-    println!("%DAY%");
+    println!("%DAY_NICE%");
     println!("{:?}", solution::part1(&input));
     println!("{:?}", solution::part2(&input));
 
