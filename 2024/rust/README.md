009578f6e4bd76a122d9cf0743af79367f81e114
sij1nk <gergo.rajnavolgyi@gmail.com>
2024-12-22 20:48:15 +0100

    2024: add day 3 solutions

--- 2024/rust/README.md
+++ 2024/rust/README.md
@@ -7,4 +7,6 @@ test day 1 part 1 ... bench:       65239 ns/iter (+/- 293)
 test day 1 part 2 ... bench:       91827 ns/iter (+/- 1459)
 test day 2 part 1 ... bench:      126525 ns/iter (+/- 490)
 test day 2 part 2 ... bench:      303034 ns/iter (+/- 3797)
+test day 3 part 1 ... bench:      126738 ns/iter (+/- 8772)
+test day 3 part 2 ... bench:      148762 ns/iter (+/- 5889)
 ```
--- 2024/rust/day03/src/solution.rs
+++ 2024/rust/day03/src/solution.rs
@@ -1,17 +1,101 @@
-pub fn part1<'a, I, S>(lines: I) -> Option<u32>
-where
-    I: IntoIterator<Item = &'a S>,
-    S: AsRef<str> + 'a,
-{
-    todo!()
+use itertools::Itertools;
+use regex::Regex;
+
+fn parse_u64(input: &str) -> anyhow::Result<u64> {
+    input.parse::<u64>().map_err(|e| e.into())
 }
 
-pub fn part2<'a, I, S>(_lines: I) -> Option<u32>
+pub fn part1<'a, I, S>(lines: I) -> anyhow::Result<u64>
 where
     I: IntoIterator<Item = &'a S>,
     S: AsRef<str> + 'a,
 {
-    todo!()
+    let input =
+        Itertools::intersperse(lines.into_iter().map(|s| s.as_ref()), " ").collect::<String>();
+
+    let regex = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+    let captures_iter = regex.captures_iter(&input);
+
+    captures_iter
+        .map(|caps| {
+            let n1 = parse_u64(&caps[1])?;
+            let n2 = parse_u64(&caps[2])?;
+
+            Ok(n1 * n2)
+        })
+        .sum()
+}
+
+#[derive(Debug)]
+struct Instruction {
+    location: usize,
+    data: InstructionData,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum InstructionData {
+    Do,
+    Dont,
+    Mul(u64, u64),
+}
+
+pub fn part2<'a, I, S>(lines: I) -> anyhow::Result<u64>
+where
+    I: IntoIterator<Item = &'a S>,
+    S: AsRef<str> + 'a,
+{
+    let input =
+        Itertools::intersperse(lines.into_iter().map(|s| s.as_ref()), " ").collect::<String>();
+
+    let mul_regex = Regex::new(r"mul\(([0-9]+),([0-9]+)\)").unwrap();
+    let do_regex = Regex::new(r"do\(\)").unwrap();
+    let dont_regex = Regex::new(r"don't\(\)").unwrap();
+
+    let mut instructions = Vec::<Instruction>::new();
+
+    let mul_instructions = mul_regex.captures_iter(&input).filter_map(|caps| {
+        let m = caps.get(0).unwrap();
+        let n1 = parse_u64(&caps[1]).ok()?;
+        let n2 = parse_u64(&caps[2]).ok()?;
+
+        Some(Instruction {
+            location: m.start(),
+            data: InstructionData::Mul(n1, n2),
+        })
+    });
+
+    let do_instructions = do_regex.find_iter(&input).map(|m| Instruction {
+        location: m.start(),
+        data: InstructionData::Do,
+    });
+
+    let dont_instructions = dont_regex.find_iter(&input).map(|m| Instruction {
+        location: m.start(),
+        data: InstructionData::Dont,
+    });
+
+    instructions.extend(mul_instructions);
+    instructions.extend(do_instructions);
+    instructions.extend(dont_instructions);
+
+    instructions.sort_by(|i1, i2| i1.location.cmp(&i2.location));
+
+    let mut sum: u64 = 0;
+    let mut enabled = true;
+
+    for ins in instructions.into_iter() {
+        match ins.data {
+            InstructionData::Do => enabled = true,
+            InstructionData::Dont => enabled = false,
+            InstructionData::Mul(n1, n2) => {
+                if enabled {
+                    sum += n1 * n2
+                }
+            }
+        }
+    }
+
+    Ok(sum)
 }
 
 #[cfg(test)]
@@ -28,10 +112,13 @@ mod tests {
         assert_eq!(result, 161);
     }
 
+    static EXAMPLE2: &[&str] =
+        &["xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"];
+
     #[test]
     fn part2_test() {
-        let result = part2(EXAMPLE).unwrap();
+        let result = part2(EXAMPLE2).unwrap();
 
-        assert_eq!(result, 0);
+        assert_eq!(result, 48);
     }
 }
